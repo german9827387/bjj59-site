68deebb6f9802f0080ae9387f7dab759b654db1f
german9827387 <shesterikov@list.ru>
2026-03-13 01:26:46 +0300

    perf: remove unused fonts.googleapis.com preconnect, add dns-prefetch for Yandex Metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,8 +96,8 @@ export default function RootLayout({
             <img src={`https://mc.yandex.ru/watch/${YM_ID}`} style={{position:"absolute",left:"-9999px"}} alt="" />
           </div>
         </noscript>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="dns-prefetch" href="https://api.vk.com" />
+        <link rel="dns-prefetch" href="https://mc.yandex.ru" />
       </head>
       <body className={`${inter.variable} antialiased min-h-screen bg-background text-foreground`}>
           <TgLinkHandler />
