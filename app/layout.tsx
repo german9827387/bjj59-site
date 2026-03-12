827b8b0ae67bd8b59028217fd35fe86d9afe6678
german9827387 <shesterikov@list.ru>
2026-03-13 01:44:52 +0300

    perf: Yandex Metrika strategy afterInteractive -> lazyOnload (loads when browser is idle)

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,7 +82,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
         {/* Яндекс Метрика */}
-        <Script id="ym-init" strategy="afterInteractive">{`
+        <Script id="ym-init" strategy="lazyOnload">{`
           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
           m[i].l=1*new Date();
           for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
